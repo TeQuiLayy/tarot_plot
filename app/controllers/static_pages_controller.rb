b00ac3431f4a81d235f005aa12586b772e7a298e
tequila <yuya.arukat@gmail.com>
2018-02-13 22:10:05 +0900

    feat: Fix html of static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    render html: "hello, world!"
-  end
-
-  def help
   end
 
   def about
+  end
+
+  def contact
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,13 +6,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test "should get contact" do
+    get static_pages_contact_url
     assert_response :success
   end
 
